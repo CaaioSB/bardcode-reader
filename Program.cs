3a622f4535858a9f4429667a301bb484ca9df369
Caio Silva <caio_silvabatista@hotmail.com>
2020-07-05 17:15:36 -0300

    Função para criar directórios padrões ao iniciar a aplicação desenvolvida.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using BarcodeReader.Common;
 using BarcodeReader.Formulario;
+using uiCSB.Toastr;
 
 namespace BarcodeReader
 {
@@ -16,9 +17,21 @@ namespace BarcodeReader
         [STAThread]
         static void Main()
         {
+            try
+            {
+                if (!App.CreateDirectory())
+                {
+                    new Alert("O directório da aplicação não existe, tente reiniciar a aplicação.", uiCSB.Toastr.Type.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                new Alert(ex.Message, uiCSB.Toastr.Type.Warning);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmGetProduto());
+            Application.Run(new frmReader());
         }
     }
 }
